71f8369af5c62364cfec7227f308763524901a60
arfindev <developerarfin@gmail.com>
2021-08-26 21:53:39 +0600

    see the change

--- app/src/main/java/com/example/movieapp_jetpack_compose/LazyColumn.kt
+++ app/src/main/java/com/example/movieapp_jetpack_compose/LazyColumn.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
@@ -15,6 +17,41 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+
+@Composable
+fun EachRow(movie: Movie) {
+    Card(
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxWidth(),
+        shape = RoundedCornerShape(
+            CornerSize(5.dp)
+        ), elevation = 2.dp
+    ) {
+        Row(modifier = Modifier.padding(5.dp)) {
+            Image(
+                painter = painterResource(id = R.mipmap.scoobydoo),
+                contentDescription = "ScoobyDoo",
+                modifier = Modifier
+                    .padding(8.dp)
+                    .size(80.dp)
+                    .align(Alignment.CenterVertically)
+                    .clip(RoundedCornerShape(CornerSize(10.dp)))
+            )
+            Text(text = movie.title, modifier = Modifier.padding(8.dp))
+        }
+    }
+}
+
+@Composable
+fun Recycleview(movie: List<Movie>) {
+    LazyColumn( ){
+        items(movie){movies ->
+            EachRow(movie = movies)
+        }
+
+    }
+}
 
 
 
