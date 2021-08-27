7540d32942a597cf54566a014eb7c05f87781b2d
arfindev <developerarfin@gmail.com>
2021-08-27 11:13:26 +0600

    Completed

--- app/src/main/java/com/example/movieapp_jetpack_compose/MainActivity.kt
+++ app/src/main/java/com/example/movieapp_jetpack_compose/MainActivity.kt
@@ -3,14 +3,23 @@ package com.example.movieapp_jetpack_compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.ui.semantics.SemanticsProperties.Text
+import androidx.compose.ui.text.input.KeyboardType.Companion.Text
+import com.example.movieapp_jetpack_compose.ComposeUi.MovieDataList
+import com.example.movieapp_jetpack_compose.ComposeUi.RecyclerView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            val MovieNames = MovieData()
-            val getAlldatas = MovieNames.getAllData()
-            Recycleview(movie = getAlldatas)
+            val MovieClass = MovieDataList()
+            val getMovieAllData = MovieClass.getMovieData()
+
+            RecyclerView(movieLists = getMovieAllData)
+
+
 
         }
     }
--- app/src/main/java/com/example/movieapp_jetpack_compose/MovieDataList.kt
+++
@@ -1,5 +0,0 @@
-package com.example.movieapp_jetpack_compose
-
-data class Movie(
-    val title: String,
-)
